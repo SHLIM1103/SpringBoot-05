3cfdc99d1679cb74039a39be0f793ede8a2eabc6
SHLIM1103 <gusl5525@gmail.com>
2020-12-15 09:29:52 +0900

    20.12.14 18:00

---
+++ src/main/java/com/example/demo/sym/service/model/TeacherDto.java
@@ -0,0 +1,12 @@
+package com.example.demo.sym.service.model;
+
+import org.springframework.stereotype.Component;
+
+import lombok.Data;
+
+@Component
+@Data
+public class TeacherDto {
+    private String teaNum, email, password;
+
+}
